d83815e022f2b947f7a380ae9f9befa7cd970496
Yuu Yonamine <yu_yonamine@ex-ex.co.jp>
2023-03-29 05:37:33 +0900

    useReducer

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,8 +1,4 @@
-import {
-  CREATE_EVENT,
-  DELETE_ALL_EVENTS,
-  DELETE_EVENT
-} from '../actions'
+import { CREATE_EVENT, DELETE_ALL_EVENTS, DELETE_EVENT } from "../actions";
 
 // action = {
 //   type: 'CREATE_EVENT',
@@ -12,7 +8,7 @@ import {
 //
 //
 // # before
-// state = []
+//状態がない場合 state = []
 //
 // # after
 // state = [
@@ -41,20 +37,21 @@ import {
 //     body: '2020年に東京でオリンピックを開催します！つきましては、、、、、、、'
 //   }
 // ]
+// どんなIDに依存するかは状態に依存する。この場合はid:3最後の状態だから。
 const events = (state = [], action) => {
-  switch(action.type) {
+  switch (action.type) {
     case CREATE_EVENT:
-      const event = { title: action.title, body: action.body }
-      const length = state.length
-      const id = length === 0 ? 1 : state[length - 1].id + 1
-      return [...state, { id, ...event }]
+      const event = { title: action.title, body: action.body };
+      const length = state.length;
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
+      return [...state, { id, ...event }];
     case DELETE_EVENT:
-      return state.filter(event => event.id !== action.id)
+      return state.filter((event) => event.id !== action.id);
     case DELETE_ALL_EVENTS:
-      return []
+      return [];
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default events
+export default events;
